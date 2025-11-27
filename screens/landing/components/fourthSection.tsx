f973c9784337b3d1f6f4028dd1f9a975bedc096d
Pongsapat <klongdigital11@gmail.com>
2025-11-27 17:26:45 +0700

    Landing-Page-thirdSection

---
+++ screens/landing/components/fourthSection.tsx
@@ -0,0 +1,11 @@
+const FourthSection = () => {
+  return (
+    <section>
+     <div className="relative size-32 ...">
+        <p className='text-2xl font-semibold  text-center'>บริการครบ</p>
+        <p className='text-2xl font-semibold  text-center'>จบในที่เดียว</p>
+     </div>
+    </section>
+  )
+}
+export default FourthSection
--- screens/landing/index.tsx
+++ screens/landing/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import FirstSection from './components/firstSection'
+import ThirdSection from './components/thirdSection'
+import FourthSection from './components/fourthSection'
 
 const ServiceScreen = () => {
     return (
         <div className='w-full '>
             <FirstSection />
+            <ThirdSection />
+            <FourthSection />
         </div>
     )
 }
